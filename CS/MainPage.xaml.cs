53078ee189d53f78f2828f0faa49fe330c402837
haco29 <haco29@gmail.com>
2016-03-23 09:43:48 +0200

    Change to 100

--- CS/MainPage.xaml.cs
+++ CS/MainPage.xaml.cs
@@ -23,7 +23,7 @@ namespace Blinky
             InitializeComponent();
 
             timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(250);
+            timer.Interval = TimeSpan.FromMilliseconds(100);
             timer.Tick += Timer_Tick;
             InitGPIO();
             if (pin != null)
